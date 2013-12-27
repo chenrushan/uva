21bdca6999d40cf07f555abb24ef8e989ff94bdd
juscodit <juscodit@gmail.com>
2013-12-27 17:43:37 +0800

    modify 10390 for UVA submission

--- 10390_bean_counting_EASY.c
+++ 10390_bean_counting_EASY.c
@@ -19,55 +19,71 @@ char line[2048];
 int
 main(int argc, char **argv)
 {
-    int i = 0;
+    int ncases = 0;
+    int i = 0, n = 0;
     char c;
 
-    fgets(line, sizeof(line) / sizeof(line[0]), stdin);
+    scanf("%d\n\n", &ncases);
 
-    for (i = 0; line[i] != '\n'; ++i) {
-        bean_cnts[line[i]] += 1;
-    }
+    for (n = 0; n < ncases; ++n) {
+        if (n != 0) {
+            printf("\n");
+        }
 
-    while (fgets(line, sizeof(line) / sizeof(line[0]), stdin) != NULL) {
-        char c = line[0];
+        memset(bean_cnts, 0, sizeof(bean_cnts));
+        memset(share_cnts, 0, sizeof(share_cnts));
+        total_guess = total_share = 0;
 
-        for (i = 2; line[i] != '\n'; ) {
-            char b = line[i];
-            i += 2;
-            int g = atoi(line + i);
+        fgets(line, sizeof(line) / sizeof(line[0]), stdin);
 
-            /* update total_guess */
-            total_guess += 1;
+        for (i = 0; line[i] != '\n'; ++i) {
+            bean_cnts[line[i]] += 1;
+        }
 
-            /* count share */
-            int diff = g - bean_cnts[b];
-            if (diff < 0) {
-                diff = -diff;
-            }
-            int s = 0;
-            if (diff == 1) {
-                s = 1;
-            } else if (diff == 0) {
-                s = 2;
-            }
-            share_cnts[c] += s;
-            total_share += s;
-
-            while (line[i] >= '0' && line[i] <= '9') {
-                i += 1;
-            }
-            if (line[i] == '\n') {
+        while (fgets(line, sizeof(line) / sizeof(line[0]), stdin) != NULL) {
+            if (line[0] == '\n') {
                 break;
             }
-            i += 1;
-        }
-    }
+            char c = line[0];
 
-    float common = total_guess * 2.0 / total_share;
-    for (c = 'A'; c <= 'Z'; ++c) {
-        if (share_cnts[c] != 0) {
-            float w = common * share_cnts[c];
-            printf("%c %.2f\n", c, w);
+            for (i = 2; line[i] != '\n'; ) {
+                char b = line[i];
+                i += 2;
+                int g = atoi(line + i);
+
+                /* update total_guess */
+                total_guess += 1;
+
+                /* count share */
+                int diff = g - bean_cnts[b];
+                if (diff < 0) {
+                    diff = -diff;
+                }
+                int s = 0;
+                if (diff == 1) {
+                    s = 1;
+                } else if (diff == 0) {
+                    s = 2;
+                }
+                share_cnts[c] += s;
+                total_share += s;
+
+                while (line[i] >= '0' && line[i] <= '9') {
+                    i += 1;
+                }
+                if (line[i] == '\n') {
+                    break;
+                }
+                i += 1;
+            }
+        }
+
+        float common = total_guess * 2.0 / total_share;
+        for (c = 'A'; c <= 'Z'; ++c) {
+            if (share_cnts[c] != 0) {
+                float w = common * share_cnts[c];
+                printf("%c %.2f\n", c, w);
+            }
         }
     }
 
